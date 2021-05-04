3605cd260f2acdf57f30a9a5fb0764ca19462d81
David <david5292@live.com>
2021-05-04 12:37:28 -0500

    added conditional for setTile

--- src/app/map-editor/map-editor.component.ts
+++ src/app/map-editor/map-editor.component.ts
@@ -193,10 +193,10 @@ export class MapEditorComponent implements OnInit, OnDestroy {
   }
 
   setTile = (x: number, y: number, v: number) => {
+    if(x < 0 && x > 20 || y < 3 && y < 32) return;
     const tile = this.editor.selectedTile;
     if (!tile.data || v === tile.data[y][x]) return;
     tile.unsaved = true;
-
     const oldTile = { x, y, v: tile.data[y][x] };
     tile.data[y][x] = v;
     this.renderer?.fillMap(tile.data, []);
