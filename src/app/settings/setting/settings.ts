f9f90bf0440a6b21f264d641ab5bf6c6ce57dbfa
Andrew <amh11706@gmail.com>
2021-05-05 03:22:12 +0200

    fix ship setting title

--- src/app/settings/setting/settings.ts
+++ src/app/settings/setting/settings.ts
@@ -14,7 +14,7 @@ export const Settings = {
     id: 19, group: 'boats', name: 'cade', type: 'boat', trigger: OutCmd.NextBoat,
     titles: [
       , , , , , , , , , , , , , , 'Sloop', 'Cutter', 'Dhow', 'Fanchuan', 'Longship', 'Baghlah', 'Merchant Brig', 'Junk',
-      'War Brig', 'Merchant Galleon', 'Xebec', 'War Frigate', 'Grand Frigate'
+      'War Brig', 'Merchant Galleon', 'Xebec', , 'War Frigate', 'Grand Frigate'
     ],
     groups: [
       { name: 'Next Ship', options: [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 26, 27] },
