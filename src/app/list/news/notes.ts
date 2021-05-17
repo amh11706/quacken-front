74907af644e5cddd1aa52976c2fd70189401b638
Andrew <amh11706@gmail.com>
2021-05-17 19:20:07 +0200

    update notes

--- src/app/list/news/notes.ts
+++ src/app/list/news/notes.ts
@@ -5,6 +5,16 @@ interface Note {
 
 export const Notes: Note[] = [
   {
+    title: 'Release 17 May 2021',
+    points: [
+      'Added sound effects!',
+      'Cannons, alerts, and more subtle notifications for tells and the sort while the tab is hidden.',
+      'Disengage is now two buttons, one for respawn island side, one for respawn ocean side.',
+      'New setting for "Always focus chat" which disables keyboard shortcuts in favor of focusing the chat.',
+      'Fixed the next move token selection to reset when you sink rather than giving you the wrong moves without showing it.'
+    ],
+  },
+  {
     title: 'Release 12 May 2021',
     points: [
       'Cade AI is alive!',
