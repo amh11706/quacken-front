18a7a31088303ec8e7d504f5fc2c58f8e546ecad
David <david5292@live.com>
2021-06-18 15:53:05 -0500

    fix generated card

--- src/app/settings/map-list/map-card/map-card.component.ts
+++ src/app/settings/map-list/map-card/map-card.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { SettingsService } from '../../settings.service';
 import { Settings } from '../../setting/settings';
-import { DBTile } from 'src/app/map-editor/map-editor.component';
+import { WsService } from 'src/app/ws.service';
+import { OutCmd } from 'src/app/ws-messages';
 
 export interface MapOption {
   id: number
@@ -21,14 +22,34 @@ export interface MapOption {
   templateUrl: './map-card.component.html',
   styleUrls: ['./map-card.component.scss']
 })
+
 export class MapCardComponent {
   @Input() map?: MapOption;
   @Input() setting?: typeof Settings['cadeMap'];
   @Output() selectedMap = new EventEmitter<number>();
   generated = 'Generated';
-  constructor(public ss : SettingsService) { }
+  seeds: string[] = [];
 
-  selectMap(id: number){
+  constructor(public ss : SettingsService, public ws: WsService) { }
+
+  selectMap(id: number) {
     this.selectedMap.emit(id);
+    this.pushSeed((document.getElementById("seed") as HTMLInputElement).value);
+  }
+
+  async addSeed(seed: string) {
+    this.pushSeed(seed);
+    this.updateSeed(seed);
+  }
+
+  pushSeed(seed: string) {
+    if (this.seeds.length > 8) this.seeds.shift();
+    if (seed !== "" && !(this.seeds.find(item => item === seed))) {
+      this.seeds.push(seed);
+    }
+  }
+
+  updateSeed(seed: string) {
+    this.ws.send(OutCmd.ChatCommand, "/seed " + seed);
   }
 }
