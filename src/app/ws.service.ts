1d67266581379a99df45d5c3e30abe88901014f3
Andrew <amh11706@gmail.com>
2022-05-29 18:44:06 +0200

    version bump

--- src/app/ws.service.ts
+++ src/app/ws.service.ts
@@ -6,7 +6,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from '../environments/environment';
 import { InCmd, Internal, OutCmd } from './ws-messages';
 
-const ClientVersion = 18;
+const ClientVersion = 19;
 
 export interface InMessage {
   cmd: InCmd | Internal;
