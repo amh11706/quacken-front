e8dc102e2ad9927055deb95c0ed84924ff23b984
Andrew <amh11706@gmail.com>
2022-07-17 15:35:06 +0200

    rename whirl sprite

--- src/app/lobby/cadegoose/twod-render/twod-render.component.ts
+++ src/app/lobby/cadegoose/twod-render/twod-render.component.ts
@@ -68,7 +68,7 @@ export class TwodRenderComponent implements OnInit, AfterViewInit, OnDestroy {
   private water = new Sprite('cell', 64, 48, [[128, 0]]);
   private sz = new Sprite('safezone', 64, 48, [[128, 0]]);
   private wind = new Sprite('wind', 64, 48, [[192, 0], [0, 0], [64, 0], [128, 0]]);
-  private whirl = new Sprite('whirl', 64, 48, [[64, 0], [128, 0], [192, 0], [0, 0], [320, 0], [384, 0], [448, 0], [256, 0]]);
+  private whirl = new Sprite('whirl2', 64, 48, [[64, 0], [128, 0], [192, 0], [0, 0], [320, 0], [384, 0], [448, 0], [256, 0]]);
 
   private wheelDebounce?: number;
   private sub = new Subscription();
