2285f1f2389c7b4e5bab845dc51dd6c25eddab9f
Andrew <amh11706@gmail.com>
2023-01-21 23:00:36 +0100

    version bump

--- src/app/ws.service.ts
+++ src/app/ws.service.ts
@@ -7,7 +7,7 @@ import { environment } from '../environments/environment';
 import { InCmd, Internal, OutCmd } from './ws-messages';
 import { AuthGuard } from './auth.guard';
 
-const ClientVersion = 36;
+const ClientVersion = 37;
 
 export interface InMessage {
   cmd: InCmd | Internal;
