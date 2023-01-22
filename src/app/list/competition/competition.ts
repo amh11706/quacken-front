e132ff43a6d9220bd3ea12a659d5338be149595f
Andrew <amh11706@gmail.com>
2023-01-22 03:07:15 +0100

    update competition maps

--- src/app/list/competition/competition.ts
+++ src/app/list/competition/competition.ts
@@ -4,7 +4,7 @@ const BaseSettings = {
   createType: { value: 2 },
   maxPlayers: { id: 24, value: 40 },
   publicMode: { id: 25, value: 0, data: 'Public' },
-  hotEntry: { id: 26, value: 1, data: 'true' },
+  hotEntry: { id: 26, value: 0, data: 'false' },
   jobberQuality: {
     id: 27,
     value: 105,
@@ -62,16 +62,21 @@ export const Competitions: Competition[] = [
           },
         ],
       },
-      // {
-      //   description: 'Loser bracket - Round 5',
-      //   games: [
-      //     {
-      //       ...BaseSettings,
-      //       name: { data: 'R5 - Loser', value: 0 },
-      //       map: { id: 18, value: 312, data: 'R5 - Loser' },
-      //     },
-      //   ],
-      // },
+      {
+        description: 'KO stage',
+        games: [
+          {
+            ...BaseSettings,
+            name: { data: 'KO R32 G1', value: 0 },
+            map: { id: 18, value: 481, data: '2023-Map4' },
+          },
+          {
+            ...BaseSettings,
+            name: { data: 'KO R16 G1', value: 0 },
+            map: { id: 18, value: 479, data: '2023-Map2' },
+          },
+        ],
+      },
       // {
       //   description: 'Loser bracket - Round 6',
       //   games: [
--- src/app/ws.service.ts
+++ src/app/ws.service.ts
@@ -7,7 +7,7 @@ import { environment } from '../environments/environment';
 import { InCmd, Internal, OutCmd } from './ws-messages';
 import { AuthGuard } from './auth.guard';
 
-const ClientVersion = 37;
+const ClientVersion = 38;
 
 export interface InMessage {
   cmd: InCmd | Internal;
