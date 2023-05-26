99e7c798b2ad8e1fcc495a4c6dbf977757d1fe42
Andrew <amh11706@gmail.com>
2023-05-27 00:01:52 +0200

    enable fg map rating

--- src/app/ws.service.ts
+++ src/app/ws.service.ts
@@ -7,7 +7,7 @@ import { environment } from '../environments/environment';
 import { InCmd, Internal, OutCmd } from './ws-messages';
 import { AuthGuard } from './auth.guard';
 
-const ClientVersion = 46;
+const ClientVersion = 47;
 
 export interface InMessage {
   cmd: InCmd | Internal;
