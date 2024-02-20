72bef6750076dcba217e4260973608e89999cf52
Andrew <amh11706@gmail.com>
2024-02-20 20:29:42 +0100

    version bump

--- src/app/ws/ws.service.ts
+++ src/app/ws/ws.service.ts
@@ -10,7 +10,7 @@ import { InputCmds, InputlessCmds, OutCmdInputTypes, OutCmdReturnTypes } from '.
 import { SendCmdInputless, SendCmdInputs } from './ws-send-types';
 import { InMessage, SubscribeData } from './ws-subscribe-types';
 
-const ClientVersion = 55;
+const ClientVersion = 56;
 
 export interface TokenUser {
   id: number;
