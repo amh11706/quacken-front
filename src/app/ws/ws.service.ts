52ade777b495329ef3de5455c4f90ddcd6d1d5af
Andrew <amh11706@gmail.com>
2024-03-03 01:17:35 +0100

    Update client version to 60

--- src/app/ws/ws.service.ts
+++ src/app/ws/ws.service.ts
@@ -10,7 +10,7 @@ import { InputCmds, InputlessCmds, OutCmdInputTypes, OutCmdReturnTypes } from '.
 import { SendCmdInputless, SendCmdInputs } from './ws-send-types';
 import { InMessage, SubscribeData } from './ws-subscribe-types';
 
-const ClientVersion = 59;
+const ClientVersion = 60;
 
 export interface TokenUser {
   id: number;
