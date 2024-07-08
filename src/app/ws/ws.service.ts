613f9dd6976f63863b8b5a186bc02d086459a593
Andrew <amh11706@gmail.com>
2024-07-08 19:43:05 +0200

    fix websocket logout triedpath

--- src/app/ws/ws.service.ts
+++ src/app/ws/ws.service.ts
@@ -46,7 +46,7 @@ export class WsService implements OnDestroy {
       this.close();
       WsService.reason = reason;
       window.localStorage.removeItem('token');
-      AuthGuard.triedPath = location.hash.substr(2);
+      AuthGuard.triedPath = location.hash.substr(2).split('?')[0] || '';
       void this.router.navigate(['auth/login']);
     });
     this.subscribe(InCmd.NavigateTo, path => {
