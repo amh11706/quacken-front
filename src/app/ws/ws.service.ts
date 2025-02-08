f6e1e0a7420b9636c629bb44caa44cf82e098e39
Andrew <amh11706@gmail.com>
2025-02-08 04:28:26 +0100

    fix: update client version to 91

--- src/app/ws/ws.service.ts
+++ src/app/ws/ws.service.ts
@@ -12,7 +12,7 @@ import { InputCmds, InputlessCmds, OutCmdInputTypes, OutCmdReturnTypes, OutReque
 import { OutMessage, SendCmdInputless, SendCmdInputs } from './ws-send-types';
 import { InMessage, SubscribeData } from './ws-subscribe-types';
 
-const ClientVersion = 90;
+const ClientVersion = 91;
 
 export interface TokenUser {
   id: number;
