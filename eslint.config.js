1b2a3dbcdf92b1e467d1acc417caa4fdca19a351
Andrew <amh11706@gmail.com>
2025-03-09 04:02:20 +0100

    enable angular template eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,43 @@
 import angular from '@angular-eslint/eslint-plugin';
+import angularTemplate from '@angular-eslint/eslint-plugin-template';
+import angularTemplateParser from '@angular-eslint/template-parser';
 import neostandard from 'neostandard';
 import unusedImports from 'eslint-plugin-unused-imports';
 import tseslint from 'typescript-eslint';
 import rxjsX from 'eslint-plugin-rxjs-x';
 
 export default [
-  // Ignore patterns
+  // Angular template files
   {
-    ignores: ['projects/**/*'],
+    files: ['**/*.html'],
+    ignores: [],
+    languageOptions: {
+      parser: angularTemplateParser,
+    },
+    plugins: {
+      '@angular-eslint/template': angularTemplate,
+    },
+    rules: {
+      ...angularTemplate.configs.recommended.rules,
+      '@angular-eslint/template/no-inline-styles': ['error', { allowBindToStyle: true }],
+      // '@angular-eslint/template/no-interpolation-in-attributes': 'error',
+      '@angular-eslint/template/attributes-order': 'error',
+    }
   },
 
   // TypeScript files
   ...neostandard({
     ts: true,
     semi: true,
+    noJsx: true,
+  }).map((config) => {
+    // don't use neostandard's ignores because it sets it globally
+    config.ignores = ['**/*.html'];
+    return config;
   }),
   ...tseslint.config(
-    tseslint.configs.recommended,
-    rxjsX.configs.recommended,
-
     {
+      files: ['**/*.ts'],
       languageOptions: {
         parser: tseslint.parser,
         parserOptions: {
@@ -27,6 +45,10 @@ export default [
           tsconfigRootDir: import.meta.dirname,
         },
       },
+      extends: [
+        rxjsX.configs.recommended,
+        tseslint.configs.recommended,
+      ],
       plugins: {
         '@angular-eslint': angular,
         'unused-imports': unusedImports,
@@ -39,6 +61,6 @@ export default [
         'accessor-pairs': 'off',
         '@angular-eslint/prefer-standalone': 'off',
       },
-    },
+    }
   ),
 ];
